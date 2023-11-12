62d3769c4f4c9f84a3d1f725ffb5dc7699ab726d
Artemov Alexandr <sasha.artemov.68@mail.ru>
2023-11-12 17:28:11 +0300

    .

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  ImageFeed
-//
-//  Created by Alexandr Artemov (Mac Mini) on 12.11.2023.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        /
     }
 
 
