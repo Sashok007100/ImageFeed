4f9936bf97f4f1f49ab10d2a8e4ef55dbecb64eb
Artemov Alexandr <sasha.artemov.68@mail.ru>
2023-12-31 14:58:19 +0300

    Устранена гонка при выполнении сетевого запроса на получение Auth Token от Unsplash.

--- ImageFeed/Services/OAuth2Service.swift
+++ ImageFeed/Services/OAuth2Service.swift
@@ -5,6 +5,9 @@ final class OAuth2Service {
     
     private let urlSession = URLSession.shared
     
+    private var task: URLSessionTask?
+    private var lastCode: String?
+    
     private (set) var authToken: String? {
         get {
             return OAuth2TokenStorage().token
@@ -15,18 +18,32 @@ final class OAuth2Service {
     }
     
     func fetchAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        assert(Thread.isMainThread)
+        if lastCode == code {
+            return
+        }
+        task?.cancel()
+        lastCode = code
+        
         let request = authTokenRequest(code: code)
         let task = object(for: request) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .success(let body):
-                let authToken = body.accessToken
-                self.authToken = authToken
-                completion(.success(authToken))
-            case .failure(let error):
-                completion(.failure(error))
+            DispatchQueue.main.async {
+                guard let self = self else { return }
+                switch result {
+                case .success(let body):
+                    let authToken = body.accessToken
+                    self.authToken = authToken
+                    completion(.success(authToken))
+                    self.task = nil
+                case .failure(let error):
+                    self.lastCode = nil
+                    completion(.failure(error))
+                }
             }
+            
         }
+        
+        self.task = task
         task.resume()
     }
 }
